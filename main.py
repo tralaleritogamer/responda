cb7df21ee2c78b124abd4970a36a03f9533f3654
tralaleritogamer <davi.artur.escola@gmail.com>
2025-11-22 13:58:37 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+nome = input("qual e seu nome ")
+idade = input("qual sua idade ")
+dia = input("qual dia nasceu ")
+print (f" seu nome e {nome} e sua idade e {idade} e o dia que nasceu foi {dia}")
+
+    
